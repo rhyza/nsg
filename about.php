6f624acf5ce6d092357557f2ac6a643254294796
Rhyza Velasco <rhyza.velasco@gmail.com>
2018-02-07 00:10:08 -0500

    Modularize call to action code

--- about.php
+++ about.php
@@ -2,6 +2,7 @@
 
 include 'html/navbar.html';
 include 'html/about.html';
+include 'html/action.html';
 include 'html/footer.html';
 
 ?>
--- services.php
+++ services.php
@@ -2,6 +2,7 @@
 
 include 'html/navbar.html';
 include 'html/services.html';
+include 'html/action.html';
 include 'html/footer.html';
 
 ?>
